053c01aa17efd014a3de612919350c3ea33dc777
Stephan Richter <srichter@srsoftware.de>
2013-08-16 15:47:52 +0200

    Arbeit an Vorbereitung der Datenbank-gestützten Erzeugung der SubstanceTreeNodes

--- src/edu/fsuj/csb/reactionnetworks/interaction/gui/MetabolicNetworkPanel.java
+++ src/edu/fsuj/csb/reactionnetworks/interaction/gui/MetabolicNetworkPanel.java
@@ -26,15 +26,9 @@ public class MetabolicNetworkPanel extends TreePanel {
   }
 
 	private SubstanceTreeNode waterNode() {
-		TreeSet<String> sNames = Tools.StringSet();
-		sNames.add("Water");
-		sNames.add("Aqua");
-		TreeSet<String> rNames = Tools.StringSet();
-		rNames.add("Reaction 1");
-		rNames.add("first reaction");
 		SubstanceTreeNode node = SubstanceTreeNode.get(123);
-		node.addChild(new ReactionTreeNode(345,rNames));
-		node.addChild(new ReactionTreeNode(346,rNames));
+		node.addChild(ReactionTreeNode.get(345));
+		node.addChild(ReactionTreeNode.get(346));
 	  return node;
   }
 	
@@ -42,7 +36,6 @@ public class MetabolicNetworkPanel extends TreePanel {
 	public void paint(Graphics g) {
 		super.paint(g);
 		Dimension dim=this.getSize();
-		TreeSet<ReactionTreeNode> rNodes=ReactionTreeNode.set();
 		moveNodeTowards(tree, new Point(dim.width/2,dim.height/2));
 		tree.paint(g, this);
 	}
--- src/edu/fsuj/csb/reactionnetworks/interaction/gui/ReactionTreeNode.java
+++ src/edu/fsuj/csb/reactionnetworks/interaction/gui/ReactionTreeNode.java
@@ -1,18 +1,33 @@
 package edu.fsuj.csb.reactionnetworks.interaction.gui;
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import de.srsoftware.gui.treepanel.TreeNode;
 import edu.fsuj.csb.tools.xml.ObjectComparator;
+import edu.fsuj.csb.tools.xml.Tools;
 
 public class ReactionTreeNode extends TreeNode {
+	static TreeMap<Integer,ReactionTreeNode> rtns=new TreeMap<Integer, ReactionTreeNode>();
 
-	public ReactionTreeNode(int id, TreeSet<String> names) {
-		super(id+"\n "+names.first());
+	public ReactionTreeNode(int id) {
+		super(id+"\n "+names(id).first());
+  }
+
+	private static TreeSet<String> names(int id) {
+		TreeSet<String> result = Tools.StringSet();
+		result.add("reaction_"+id);
+		return result;
   }
 
 	public static TreeSet<ReactionTreeNode> set() {
 	  return new TreeSet<ReactionTreeNode>(ObjectComparator.get());
+  }
+
+	public static ReactionTreeNode get(int id) {
+		ReactionTreeNode result = rtns.get(id);
+		if (result==null) result=new ReactionTreeNode(id);
+	  return result;
   }
 
 }
